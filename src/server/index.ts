3bae7be3f6d7659aa5b6804260ed880d9fbbec2a
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-06-08 23:14:18 +0100

    move examples to other module

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,41 +1,6 @@
 require("dotenv").config();
 //
-import { notion } from "./notion";
-import { getBlocks } from "./notion/utils";
 
-async function main() {
-  // Get databases
-  const dbs = await notion.databases.list({});
-  console.log(JSON.stringify(dbs, null, 2));
-
-  // Get database pages
-  const query = await notion.databases.query({
-    database_id: "6cf88cb0-e6fc-446d-bae2-27116b7943e9",
-  });
-  console.log(JSON.stringify(query, null, 2));
-
-  // Search notion
-  console.log(JSON.stringify(await notion.search({}), null, 2));
-
-  // Get page
-  console.log(
-    JSON.stringify(
-      await notion.pages.retrieve({
-        page_id: "01ec02e3-b936-46c4-86e9-743311560a4f",
-      }),
-      null,
-      2
-    )
-  );
-
-  // Get blocks
-  console.log(
-    JSON.stringify(
-      await getBlocks("01ec02e3-b936-46c4-86e9-743311560a4f"),
-      null,
-      2
-    )
-  );
-}
+async function main() {}
 
 main();
